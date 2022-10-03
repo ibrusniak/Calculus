30142f63ceb8278f093ab20614409fda65a6fdd5
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-10-03 09:19:52 +0300

    PET. Calculator. Under contruction

--- Calculator/Test.java
+++ Calculator/Test.java
@@ -6,19 +6,24 @@ public class Test {
 
     public static void main(String[] args) {
 
-        NumberEntity ne = NumberEntity.from("156.255");
+        NumberEntity a = NumberEntity.from("00000.00000");
+        NumberEntity b = NumberEntity.from(0);
 
-        System.out.println(ne);
-        System.out.println(ne.toDebugString());
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println(a.compareTo(b));
     }
 }
 
-class NumberEntity implements Cloneable {
+class NumberEntity implements Cloneable, Comparable<NumberEntity> {
 
     private ArrayDeque<Byte> intPart = new ArrayDeque<>();
     private ArrayDeque<Byte> decPart = new ArrayDeque<>();
     private boolean integer = true;
     private boolean positive = true;
+
+    private NumberEntity() {}
 
     public NumberEntity push(int digit) {
 
@@ -113,6 +118,19 @@ class NumberEntity implements Cloneable {
             || (intPart.stream().allMatch(x -> x.equals("0")) && decPart.isEmpty());
     }
 
+    public int compareTo(NumberEntity another) {
+
+        int result = 0;
+        if (positive != another.positive) {
+            result = positive ? 1 : -1;
+        } else if (positive && another.positive) {
+            result = compareToByModule(this, another);
+        } else {
+            result = -1 * compareToByModule(this, another);
+        }
+        return result;
+    }
+
     public static NumberEntity from(int i) throws IllegalArgumentException {
         return from(String.valueOf(i));
     }
@@ -126,7 +144,7 @@ class NumberEntity implements Cloneable {
 
     public static NumberEntity from(String s) throws IllegalArgumentException {
 
-        if (!s.matches("(^[-+]{0,1}\\d*\\.\\d*$)|(^[-+]{0,1}\\d*$)") || s.matches("(^0+\\.0+$)|(^0+$)"))
+        if (s == "" || !s.matches("(^[-+]{0,1}\\d*\\.\\d*$)|(^[-+]{0,1}\\d*$)"))
             throw new IllegalArgumentException(s);
         NumberEntity ne = new NumberEntity();
         if (s.contains("-")) ne.resetPositiveFlag();
@@ -166,13 +184,45 @@ class NumberEntity implements Cloneable {
         Optional<String> o = decPart.stream().map(x -> String.valueOf(x)).reduce((x, y) -> x + y);
         return o.isEmpty() ? "" : o.get();
     }
+
+    private NumberEntity padWithZeroesToWidth(int intPartWidth, int decPartWidth) throws IllegalArgumentException {
+
+        if (intPartWidth < intPart.size())
+            throw new IllegalArgumentException("Argument 'intPartWidth' is to small!");
+        if (decPartWidth < decPart.size())
+            throw new IllegalArgumentException("Argument 'decPartWidth' is to small!");
+        for (int i = intPart.size(); i < intPartWidth; i++)
+            intPart.addFirst((byte)0);
+        for (int i = decPart.size(); i < decPartWidth; i++)
+            decPart.addLast((byte)0);
+        return this;
+    }
+
+    private int compareToByModule(NumberEntity first, NumberEntity second) {
+
+        NumberEntity a = first.clone();
+        NumberEntity b = second.clone();
+        a.removeRedundandZeroes().padWithZeroesToWidth(Integer.max(a.intPart.size(), b.intPart.size()),
+            Integer.max(a.decPart.size(), b.decPart.size()));
+        b.removeRedundandZeroes().padWithZeroesToWidth(Integer.max(a.intPart.size(), b.intPart.size()),
+            Integer.max(a.decPart.size(), b.decPart.size()));
+        while (a.intPart.size() > 0) {
+            byte b1 = a.intPart.pollFirst();
+            byte b2 = b.intPart.pollFirst();
+            if (b1 > b2) return 1;
+            if (b1 < b2) return -1;
+        }
+        while (a.decPart.size() > 0) {
+            byte b1 = a.decPart.pollFirst();
+            byte b2 = b.decPart.pollFirst();
+            if (b1 > b2) return 1;
+            if (b1 < b2) return -1;
+        }
+        return 0;
+    }
 }
 
 class NumberEntityOperations {
-
-    public static int compare (NumberEntity ne1, NumberEntity ne2) {
-
-        return 0;
-    }
+    
 }
 

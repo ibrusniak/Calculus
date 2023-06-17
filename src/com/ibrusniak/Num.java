66daab7d572d6c75904ce4ee9cc99f47736428a7
Ivan Brusniak <ivan.brusniak@gmail.com>
2023-06-17 16:16:13 +0300

    Write 'Num.java'

--- src/com/ibrusniak/Num.java
+++ src/com/ibrusniak/Num.java
@@ -2,7 +2,7 @@ package com.ibrusniak;
 
 import java.util.ArrayDeque;
 
-public class Num {
+public class Num implements Comparable<Num> {
 
     private ArrayDeque<Integer> integerPart = new ArrayDeque<>();
     private ArrayDeque<Integer> fractionalPart = new ArrayDeque<>();
@@ -42,5 +42,36 @@ public class Num {
 
         return (integerPart.isEmpty() ? "0" : integerPart.toString())
             + (fractionalPart.isEmpty() ? "" : "." + fractionalPart.toString());
+    }
+
+    @Override
+    public int compareTo(Num o) {
+        
+        if (integerPart.size() != o.integerPart.size())
+            return Integer.compare(integerPart.size(), o.integerPart.size());
+        return Integer.compare(fullWeight(), o.fullWeight());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (!(obj instanceof Num)) return false;
+        return compareTo((Num)obj) == 0;
+    }
+
+    private int weight(ArrayDeque<Integer> integerOrFractionalPart) {
+        
+        int weight = 0;
+        if (!integerOrFractionalPart.isEmpty()) {
+            Object[] array = integerPart.toArray();
+            for (int i = 0, positionWeight = array.length - 1; i < array.length; i++, positionWeight--) {
+                weight += positionWeight * ((int)array[i]);
+            }
+        }
+        return weight;
+    }
+
+    private int fullWeight() {
+        return weight(integerPart) + weight(fractionalPart);
     }
 }

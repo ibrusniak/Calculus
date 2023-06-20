b1d1ac7d983da8b3e97fb7bbdaf885cb28989831
Ivan Brusniak <ivan.brusniak@gmail.com>
2023-06-20 22:07:06 +0300

    Write 'Engine.java'

--- src/com/ibrusniak/core/Engine.java
+++ src/com/ibrusniak/core/Engine.java
@@ -48,19 +48,103 @@ public final class Engine {
         }
     }
 
-    public void backspace() {
+    /**
+     * Digit keys:
+     *  Key.D0, Key.D1, Key.D2, Key.D3,
+     *  Key.D4, Key.D5, Key.D6, Key.D7,
+     *  Key.D8, Key.D9
+     * 
+     * @param key
+     */
+    private void keyPressDigitKeys(Key key) {
 
-        
+        if (key == Key.D0) {d0();}
+        else if (key == Key.D1) {d1();}
+        else if (key == Key.D2) {d2();}
+        else if (key == Key.D3) {d3();}
+        else if (key == Key.D4) {d4();}
+        else if (key == Key.D5) {d5();}
+        else if (key == Key.D6) {d6();}
+        else if (key == Key.D7) {d7();}
+        else if (key == Key.D8) {d8();}
+        else if (key == Key.D9) {d9();}
+        else {
+            throw new Error(String.format("Wrong key for function \"keyPressDigitKeys\": {%s}!", key));
+        }
     }
 
-    private void keyPressDigitKeys(Key key) {}
+    /**
+     * Operation keys:
+     * Key.PLS, Key.MNS, Key.MUL, Key.DIV, Key.PER
+     * 
+     * @param key
+     */
+    private void keyPressOperationKeys(Key key) {
 
-    private void keyPressOperationKeys(Key key) {}
+        if (key == Key.PLS) {plus();}
+        else if (key == Key.MNS) {minus();}
+        else if (key == Key.MUL) {mult();}
+        else if (key == Key.DIV) {div();}
+        else if (key == Key.PER) {persent();}
+        else {
+            throw new Error(String.format("Wrong key for function \"keyPressOperationKeys\": {%s}!", key));
+        }
+    }
 
-    private void keyPressControlKeys(Key key) {}
+    /**
+     * Control keys:
+     * Key.DOT, Key.RES, Key.BS, Key.CE,
+     * Key.C, Key.NEG, Key.MP, Key.MC, Key.MM, Key.MR
+     * 
+     * @param key
+     */
+    private void keyPressControlKeys(Key key) {
+
+        if (key == Key.DOT) {dot();}
+        else if (key == Key.RES) {result();}
+        else if (key == Key.BS) {backspace();}
+        else if (key == Key.CE) {cleareverything();}
+        else if (key == Key.C) {clear();}
+        else if (key == Key.NEG) {negative();}
+        else if (key == Key.MP) {memoryplus();}
+        else if (key == Key.MC) {memoryclear();}
+        else if (key == Key.MM) {memoryminus();}
+        else if (key == Key.MR) {mr();}
+        else {
+            throw new Error(String.format("Wrong key for function \"keyPressControlKeys\": {%s}!", key));
+        }
+    }
 
     @Override
     public String toString() {
         return buffer.toString();
     }
+
+    private void dot() {}
+    private void result() {}
+    private void backspace() {}
+    private void cleareverything() {}
+    private void clear() {}
+    private void negative() {}
+    private void memoryplus() {}
+    private void memoryclear() {}
+    private void memoryminus() {}
+    private void mr() {}
+
+    private void plus() {}
+    private void minus() {}
+    private void mult() {}
+    private void div() {}
+    private void persent() {}
+
+    private void d0() {}
+    private void d1() {}
+    private void d2() {}
+    private void d3() {}
+    private void d4() {}
+    private void d5() {}
+    private void d6() {}
+    private void d7() {}
+    private void d8() {}
+    private void d9() {}
 }

73b38ae34b5e33221554ae72b82d5f61a8797dc9
Ivan Brusniak <ivan.brusniak@gmail.com>
2023-06-21 16:45:21 +0300

    Writ 'Processor.java'

---
+++ src/com/ibrusniak/core/Processor.java
@@ -0,0 +1,36 @@
+package com.ibrusniak.core;
+
+import java.util.ArrayDeque;
+
+public class Processor {
+    
+    public ArrayDeque<String> makeAddition(ArrayDeque<String> op1, ArrayDeque<String> op2) {
+
+        ArrayDeque<String> result = new ArrayDeque<>();
+        return result;
+    }
+
+    public ArrayDeque<String> makeSubtraction(ArrayDeque<String> op1, ArrayDeque<String> op2) {
+
+        ArrayDeque<String> result = new ArrayDeque<>();
+        return result;
+    }
+
+    public ArrayDeque<String> makeMultiplication(ArrayDeque<String> op1, ArrayDeque<String> op2) {
+
+        ArrayDeque<String> result = new ArrayDeque<>();
+        return result;
+    }
+
+    public ArrayDeque<String> makeDivision(ArrayDeque<String> op1, ArrayDeque<String> op2) {
+
+        ArrayDeque<String> result = new ArrayDeque<>();
+        return result;
+    }
+
+    private ArrayDeque<String> makeModulo(ArrayDeque<String> op) {
+
+        ArrayDeque<String> result = new ArrayDeque<>();
+        return result;
+    }
+}
